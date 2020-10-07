32fef9a72c712543c94cd471316fb2fa4addd650
Cian O Mahony <CIAN@Cians-MacBook-Pro.local>
2020-10-07 11:10:42 +0100

    changing

--- config.py
+++
@@ -1,8 +0,0 @@
-DB_HOST = 'ec2-54-91-178-234.compute-1.amazonaws.com'
-DB_NAME = 'dcv893e56jvrev'
-DB_USER = 'rddrpvwnixzwgo'
-DB_PASS = 'b3272dfa892a6487b8593c78838c11aa3686713333727d557a7792ebef0e044b'
-DB_PORT = 5432
-
-EMAIL_PASSWORD = 'jfxsixrwyivazpbw'
-API_KEY = '28c500e859576a4f8bbcbe09c83e2492'
